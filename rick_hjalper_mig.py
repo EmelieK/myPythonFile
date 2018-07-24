3cf1fd7e43e796ec0650af28467a241ec3e8e3f6
emkva <emelie.kvarnstromhotmail.se>
2018-07-24 21:43:28 +0200

    Playing with nano

--- rick_hjalper_mig.py
+++ rick_hjalper_mig.py
@@ -1,1 +1,5 @@
 print("Hej Ricky! :)\nVar chia-puddingen god?")
+def squared(x):
+  retun (x^^2)
+
+print(squared(5))
---
+++ ricky_hjalper_mig.py
@@ -0,0 +1,15 @@
+print("Hej Ricky! :)\nVar chia-puddingen god?")
+
+
+
+
+def squared(x):
+  return (x**2)
+
+
+def math(s):
+  print("The square root of " + str(s)
+ + " is " + str(squared(s)) )
+  return squared(s)
+
+print(math(7))
